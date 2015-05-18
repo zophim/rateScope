c74c4a0c02812ed55e4cf4b6ccb8e005fe6b0665
Tom Schmidt <z0phim@yahoo.com>
2015-05-18 11:46:48 -0700

    [Feature] Added per-view caching.

--- ratescope/views.py
+++ ratescope/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
+@cache_page(60*5)
+@vary_on_cookie
 def index(request):
     context = {}
     context['objects'] = []
