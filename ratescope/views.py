228cb247ed0e39a45e12cc8801e32e090c4f54ce
Tom Schmidt <z0phim@yahoo.com>
2015-05-19 15:42:32 -0700

    Added low level caching to database call on / page.

--- ratescope/views.py
+++ ratescope/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.core.cache import cache
 
 def index(request):
     context = {}
+    objects = cache.get('objects_' + request.user.username + '_key')
+    if not objects:
+        objects = []
+        cache.set('objects_' + request.user.username + '_key', 60 * 5)
     context['objects'] = []
     return render(request, 'index.html', context)
 
